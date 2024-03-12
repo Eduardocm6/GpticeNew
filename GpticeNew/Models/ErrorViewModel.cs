e36f991c83f82409fc99368c05f3f22d32dac6af
Eduardocm6 <edaxel96@gmail.com>
2024-03-12 17:42:16 -0600

    Crear Proyecto

---
+++ GpticeNew/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GpticeNew.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
